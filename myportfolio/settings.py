39c9da118a06bb681ad7f223735265d487a9cf66
Akshay Patidar <akshay.patidar111@gmail.com>
2021-08-06 22:33:34 +0530

    make it better

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
