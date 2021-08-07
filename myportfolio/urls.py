362f8477286b33a3a1aa50d9438dd2f3c147b8c7
Akshay Patidar <akshay.patidar111@gmail.com>
2021-08-07 16:13:56 +0530

    changes made

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.static import serve
-from django.conf.urls.static import url
+from django.conf.urls import url
 from django.conf import settings
 
 urlpatterns = [
